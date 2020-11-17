d348223ee4669e69aeadfbeb0603616e77e49743
hypark <HayeonP>
2020-11-17 16:35:55 +0900

    /fake_object_generator load objects as a 2d format from yaml files. Rviz setup of fake objects is changed ( Polygon -> PolygonArray )

--- rubis_ws/src/test_package/src/fake_object_generator.cpp
+++ rubis_ws/src/test_package/src/fake_object_generator.cpp
@@ -7,191 +7,130 @@
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Vector3.h>
 #include <geometry_msgs/PolygonStamped.h>
+#include <jsk_recognition_msgs/PolygonArray.h>
 #include <geometry_msgs/Point32.h>
 #include <autoware_msgs/DetectedObject.h>
 #include <autoware_msgs/DetectedObjectArray.h>
 
-
-
+static std::vector<std::string> type_names;
+void init_type_names();
 
 std_msgs::Header create_header(int seq, std::string frame_id);
 
 int main(int argc, char* argv[]){
+    init_type_names();
+
     // Initialize
     ros::init(argc, argv, "fake_object_generator");
     ros::NodeHandle nh;
     ros::Rate rate(10);
-    ros::Publisher fake_object_pub;
-    ros::Publisher polygon_pub1;
-    ros::Publisher polygon_pub2;
-    // ros::Publisher bb_pub1;
-    // ros::Publisher bb_pub2;
+    ros::Publisher object_array_pub;
+    ros::Publisher polygon_array_pub;    
+    
+    object_array_pub = nh.advertise<autoware_msgs::DetectedObjectArray>("/tracked_objects", 10);
+    polygon_array_pub = nh.advertise<jsk_recognition_msgs::PolygonArray>("/fake_polygons", 10);
 
+    // Load Parameters
+    XmlRpc::XmlRpcValue xml_object_list;
+    nh.getParam("/fake_object_generator/object_list", xml_object_list);
+    std::vector< std::vector<double> > object_list;
+    for(int i=0; i<xml_object_list.size(); i++){
+        XmlRpc::XmlRpcValue xml_object = xml_object_list[i];
+        std::vector<double> object;
+        for(int j=0; j<xml_object.size(); j++)
+            object.push_back((double)(xml_object[j]));
+        object_list.push_back(object);
+    }
 
-    fake_object_pub = nh.advertise<autoware_msgs::DetectedObjectArray>("/tracked_objects", 10);
-    polygon_pub1 = nh.advertise<geometry_msgs::PolygonStamped>("/fake_polygon1", 10);
-    polygon_pub2 = nh.advertise<geometry_msgs::PolygonStamped>("/fake_polygon2", 10);
-    int obstacle_num;
-    std::vector<float> convex_hull_param1;
-    std::vector<float> convex_hull_param2;
-    std::string frame_id;
-    nh.getParam("/fake_object_generator/obstacle_num", obstacle_num);
-    nh.getParam("/fake_object_generator/convex_hull1", convex_hull_param1);
-    nh.getParam("/fake_object_generator/convex_hull2", convex_hull_param2);    
+    std::string frame_id = "/none";
     nh.getParam("/fake_object_generator/frame_id", frame_id);
-    
 
-    std::vector<geometry_msgs::Point32> convex_hull_points1;
-    std::vector<geometry_msgs::Point32> convex_hull_points2;
-    std::vector<float> x_vec1;
-    std::vector<float> y_vec1;
-    std::vector<float> z_vec1;
-    std::vector<float> x_vec2;
-    std::vector<float> y_vec2;
-    std::vector<float> z_vec2;
+    autoware_msgs::DetectedObjectArray object_array_msg;
+    jsk_recognition_msgs::PolygonArray polygon_array_msg;
+    int object_id = 415;
 
-    for(auto it = convex_hull_param1.begin(); it != convex_hull_param1.end(); ++it){
-        int idx = it - convex_hull_param1.begin();
-        geometry_msgs::Point32 point;
-        if(idx%3 == 0){
-            x_vec1.push_back(*it);
-        }    
-        else if(idx%3 == 1){
-            y_vec1.push_back(*it);
-        }
-        else if(idx%3 == 2){
-            z_vec1.push_back(*it);
+    for(auto itlist = object_list.begin(); itlist != object_list.end(); ++itlist){
+        std::vector<double> object = *itlist;
+        std::vector<double> x_vec;
+        std::vector<double> y_vec;
+        std::vector<double> z_vec;
+        std::vector<geometry_msgs::Point32> convex_hull_points;
+        autoware_msgs::DetectedObject object_msg;
+        geometry_msgs::PolygonStamped polygon_msg;
+
+        for(auto itobj = object.begin(); itobj != object.end(); ++itobj){            
             geometry_msgs::Point32 point;
-            point.x = x_vec1[idx/3];
-            point.y = y_vec1[idx/3];
-            point.z = z_vec1[idx/3];    
-            convex_hull_points1.push_back(point);
+            int obj_idx = itobj - object.begin();
+
+            if(obj_idx%3 == 0)
+                x_vec.push_back(*itobj);
+            else if(obj_idx%3 == 1)
+                y_vec.push_back(*itobj);
+            else if(obj_idx%3 == 2){
+                z_vec.push_back(*itobj);
+                geometry_msgs::Point32 point;
+                point.x = x_vec[obj_idx/3];
+                point.y = y_vec[obj_idx/3];
+                point.z = z_vec[obj_idx/3];    
+                convex_hull_points.push_back(point);
+            }
         }
-    }
 
-    for(auto it = convex_hull_param2.begin(); it != convex_hull_param2.end(); ++it){
-        int idx = it - convex_hull_param2.begin();
-        geometry_msgs::Point32 point;
-        if(idx%3 == 0){
-            x_vec2.push_back(*it);
-        }    
-        else if(idx%3 == 1){
-            y_vec2.push_back(*it);
+        for(auto itconvex = convex_hull_points.begin(); itconvex != convex_hull_points.end(); ++itconvex){
+            geometry_msgs::Point32 point = *itconvex;
+            object_msg.convex_hull.polygon.points.push_back(*itconvex);
         }
-        else if(idx%3 == 2){
-            z_vec2.push_back(*it);
-            geometry_msgs::Point32 point;
-            point.x = x_vec2[idx/3];
-            point.y = y_vec2[idx/3];
-            point.z = z_vec2[idx/3];    
-            convex_hull_points2.push_back(point);
-        }
+
+        object_msg.id = object_id++;
+        // Pose
+        float max_x, min_x, max_y, min_y, max_z, min_z;
+        max_x = *std::max_element(x_vec.begin(), x_vec.end());
+        min_x = *std::min_element(x_vec.begin(), x_vec.end());
+        max_y = *std::max_element(y_vec.begin(), y_vec.end());
+        min_y = *std::min_element(y_vec.begin(), y_vec.end());
+        max_z = *std::max_element(z_vec.begin(), z_vec.end());
+        min_z = *std::min_element(z_vec.begin(), z_vec.end());
+        
+        object_msg.pose.position.x = min_x + (max_x-min_x)/2.0;
+        object_msg.pose.position.y = min_y + (max_y-min_y)/2.0;
+        object_msg.pose.position.z = min_z + (max_z-min_z)/2.0;
+        object_msg.pose.orientation.x = 0;
+        object_msg.pose.orientation.y = 0;
+        object_msg.pose.orientation.z = 0;
+        object_msg.pose.orientation.w = 0;
+        // Dimension
+        object_msg.dimensions.x = (max_x-min_x)/2.0;
+        object_msg.dimensions.y = (max_y-min_y)/2.0;
+        object_msg.dimensions.z = (max_z-min_z)/2.0;
+        // Velocity
+        object_msg.velocity.linear.x = 0.5;
+        object_msg.velocity.linear.y = 0.5;
+        object_msg.velocity.linear.z = 0.5;
+        // Polygon
+        polygon_msg = object_msg.convex_hull;
+        
+        // Create Msg
+        object_array_msg.objects.push_back(object_msg);
+        polygon_array_msg.polygons.push_back(polygon_msg);
     }
-    
-    // Create Fake Object
-    autoware_msgs::DetectedObjectArray fake_object_array;
-    autoware_msgs::DetectedObject fake_object1;
-    autoware_msgs::DetectedObject fake_object2;
-    
-    // ID
-    fake_object1.id = 415;
-
-    // Convex Hull
-    for(auto it = convex_hull_points1.begin(); it != convex_hull_points1.end(); ++it){
-        geometry_msgs::Point32 point = *it;
-        fake_object1.convex_hull.polygon.points.push_back(*it);        
-    }
-
-    // Pose
-    float max_x, min_x, max_y, min_y, max_z, min_z;
-    max_x = *std::max_element(x_vec1.begin(), x_vec1.end());
-    min_x = *std::min_element(x_vec1.begin(), x_vec1.end());
-    max_y = *std::max_element(y_vec1.begin(), y_vec1.end());
-    min_y = *std::min_element(y_vec1.begin(), y_vec1.end());
-    max_z = *std::max_element(z_vec1.begin(), z_vec1.end());
-    min_z = *std::min_element(z_vec1.begin(), z_vec1.end());
-
-    fake_object1.pose.position.x = min_x + (max_x-min_x)/2.0;
-    fake_object1.pose.position.y = min_y + (max_y-min_y)/2.0;
-    fake_object1.pose.position.z = min_z + (max_z-min_z)/2.0;
-    fake_object1.pose.orientation.x = 0;
-    fake_object1.pose.orientation.y = 0;
-    fake_object1.pose.orientation.z = 0;
-    fake_object1.pose.orientation.w = 0;
-    // Dimension
-    fake_object1.dimensions.x = (max_x-min_x)/2.0;
-    fake_object1.dimensions.y = (max_y-min_y)/2.0;
-    fake_object1.dimensions.z = (max_z-min_z)/2.0;
-    // Velocity
-    fake_object1.velocity.linear.x = 0.5;
-    fake_object1.velocity.linear.y = 0.5;
-    fake_object1.velocity.linear.z = 0.5;
-    // Polygon
-    geometry_msgs::PolygonStamped polygon1;
-    polygon1 = fake_object1.convex_hull;
-
-    //////////////////////////
-    
-    // ID
-    fake_object2.id = 416;
-
-    // Convex Hull
-    for(auto it = convex_hull_points2.begin(); it != convex_hull_points2.end(); ++it){
-        geometry_msgs::Point32 point = *it;
-        fake_object2.convex_hull.polygon.points.push_back(*it);        
-    }
-
-    // Pose
-    max_x = *std::max_element(x_vec2.begin(), x_vec2.end());
-    min_x = *std::min_element(x_vec2.begin(), x_vec2.end());
-    max_y = *std::max_element(y_vec2.begin(), y_vec2.end());
-    min_y = *std::min_element(y_vec2.begin(), y_vec2.end());
-    max_z = *std::max_element(z_vec2.begin(), z_vec2.end());
-    min_z = *std::min_element(z_vec2.begin(), z_vec2.end());
-
-    fake_object2.pose.position.x = min_x + (max_x-min_x)/2.0;
-    fake_object2.pose.position.y = min_y + (max_y-min_y)/2.0;
-    fake_object2.pose.position.z = min_z + (max_z-min_z)/2.0;
-    fake_object2.pose.orientation.x = 0;
-    fake_object2.pose.orientation.y = 0;
-    fake_object2.pose.orientation.z = 0;
-    fake_object2.pose.orientation.w = 0;
-
-    // Dimension
-    fake_object2.dimensions.x = (max_x-min_x)/2.0;
-    fake_object2.dimensions.y = (max_y-min_y)/2.0;
-    fake_object2.dimensions.z = (max_z-min_z)/2.0;
-    
-    // Velocity
-    fake_object2.velocity.linear.x = 0.5;
-    fake_object2.velocity.linear.y = 0.5;
-    fake_object2.velocity.linear.z = 0.5;
-
-    // Polygon
-    geometry_msgs::PolygonStamped polygon2;
-    polygon2 = fake_object2.convex_hull;
 
     int seq = 0;
-
-
-
-    while(1){        
-        fake_object1.header = create_header(seq, frame_id);
-        fake_object2.header = fake_object1.header;
-        fake_object_array.objects.clear();
-        fake_object_array.objects.push_back(fake_object1);
-        fake_object_array.objects.push_back(fake_object2);
-        fake_object_array.header = fake_object1.header;
-        polygon1.header = fake_object1.header;
-        polygon2.header = fake_object2.header;
-
-        fake_object_pub.publish(fake_object_array);
-        polygon_pub1.publish(polygon1);
-        polygon_pub2.publish(polygon2);
+    while(1){
+        std_msgs::Header header = create_header(seq, frame_id);
+        object_array_msg.header = header;
+        for(auto it = object_array_msg.objects.begin(); it != object_array_msg.objects.end(); ++it)
+            (*it).header = header;
+        polygon_array_msg.header = header;
+        for(auto it = polygon_array_msg.polygons.begin(); it != polygon_array_msg.polygons.end(); ++it){
+            (*it).header = header;            
+        }
+            
+        object_array_pub.publish(object_array_msg);
+        polygon_array_pub.publish(polygon_array_msg);
         seq++;
         rate.sleep();
     }
-    
+
 
     return 0;
 }
@@ -203,3 +142,18 @@ std_msgs::Header create_header(int seq, std::string frame_id){
     header.frame_id = frame_id;
     return header;
 }
+
+
+
+void init_type_names(){
+    type_names.push_back(std::string("invalid"));
+    type_names.push_back(std::string("bool"));
+    type_names.push_back(std::string("int"));
+    type_names.push_back(std::string("double"));
+    type_names.push_back(std::string("string"));
+    type_names.push_back(std::string("data_time"));
+    type_names.push_back(std::string("base64"));
+    type_names.push_back(std::string("array"));
+    type_names.push_back(std::string("struct"));
+}
+
