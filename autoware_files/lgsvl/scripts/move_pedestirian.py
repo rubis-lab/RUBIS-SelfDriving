906e265ce52771a404c8fa4612431b90c7ea2489
ByungKyu Park <babagom11@naver.com>
2020-12-16 20:56:38 +0900

    add python scripts and unity_map on testbed branch

--- autoware_files/lgsvl/scripts/move_pedestirian.py
+++ autoware_files/lgsvl/scripts/move_pedestirian.py
@@ -15,10 +15,10 @@ sim = lgsvl.Simulator(os.environ.get("SIMULATOR_HOST", "127.0.0.1"), 8181)
 layer_mask = 0
 layer_mask |= 1 << 0  # 0 is the layer for the road (default)
 
-if sim.current_scene == "BorregasAve":
+if sim.current_scene == "testbed":
   sim.reset()
 else:
-  sim.load("BorregasAve")
+  sim.load("testbed")
 
 spawns = sim.get_spawn()
 forward = lgsvl.utils.transform_to_forward(spawns[0])
@@ -37,7 +37,7 @@ ego.connect_bridge(os.environ.get("BRIDGE_HOST", "127.0.0.1"), 9090)
 waypoints = []
 
 #set start waypoint of cross walk
-start = spawns[0].position + 68 * forward + 6 * right
+start = spawns[0].position + 81 * forward + 8 * right
 
 hit = sim.raycast(start, lgsvl.Vector(0, -1, 0), layer_mask)
 #you can change trigger_distance what you want
@@ -45,7 +45,7 @@ wp = lgsvl.WalkWaypoint(position=hit.point, speed=1, idle=0, trigger_distance=30
 waypoints.append(wp)
 
 #set end waypoint of cross walk
-end = start - 18 * right
+end = start - 23 * right
 
 hit = sim.raycast(end, lgsvl.Vector(0, -1, 0), layer_mask)
 wp = lgsvl.WalkWaypoint(position=hit.point, speed=1, idle=0, trigger_distance=0, trigger=None)
@@ -53,7 +53,7 @@ waypoints.append(wp)
 
 #set position of pedestrian
 state = lgsvl.AgentState()
-state.transform.position = spawns[0].position + 68 * forward + 8 * right
+state.transform.position = spawns[0].position + 81 * forward + 10 * right
 state.transform.rotation = spawns[0].rotation
 
 pedestrian = sim.add_agent("Bob", lgsvl.AgentType.PEDESTRIAN, state)
