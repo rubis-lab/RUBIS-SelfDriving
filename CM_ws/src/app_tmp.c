4586d6e79f49ed00ffe43e470b4a7415834884c7
HayeonP <kite9240@gmail.com>
2021-02-04 21:04:17 +0900

    imu launch is added

--- CM_ws/src/app_tmp.c
+++ CM_ws/src/app_tmp.c
@@ -61,10 +61,10 @@ static const char *CompileFlags[] = {
 
 tAppStartInfo   AppStartInfo = {
     "Car_Generic <insert.your.version.no>",          /* App_Version         */
-    "15",          /* App_BuildVersion    */
-    "akyeast",     /* App_CompileUser     */
-    "akyeast-system-product-name",         /* App_CompileSystem   */
-    "2021-02-04 16:02:36",  /* App_CompileTime */
+    "16",          /* App_BuildVersion    */
+    "rubis",     /* App_CompileUser     */
+    "rubis-com",         /* App_CompileSystem   */
+    "2021-02-04 16:23:59",  /* App_CompileTime */
 
     CompileFlags,                /* App_CompileFlags  */
     CompileLibs,                 /* App_Libs          */
