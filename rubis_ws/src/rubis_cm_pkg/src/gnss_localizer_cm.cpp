a4094a365e8079711423f56064bd5a4cf9912e5b
HayeonP <kite9240@gmail.com>
2021-02-10 04:51:16 +0900

    GPS offset is updated to gnss cm localizer

--- rubis_ws/src/rubis_cm_pkg/src/gnss_localizer_cm.cpp
+++ rubis_ws/src/rubis_cm_pkg/src/gnss_localizer_cm.cpp
@@ -1,11 +1,12 @@
 #include "gnss_localizer_cm.h"
+
 void GPSCallback(const hellocm_msgs::GPS_Out& msg){
     current_time_ = msg.header.stamp.sec + msg.header.stamp.nsec / 1e9;
     cur_pose_.header = msg.header;
     cur_pose_.header.stamp = ros::Time::now();
     cur_pose_.header.frame_id = MAP_FRAME_;
     cur_pose_.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(roll_, pitch_, yaw_);
-    LLH2UTM(msg.latitude, msg.longitude, msg.altitude);
+    LLH2UTM(msg.latitude, msg.longitude, msg.altitude, cur_pose_);
     pose_pub.publish(cur_pose_);
 
     publishVelocity();
@@ -24,7 +25,8 @@ void IMUCallback(const sensor_msgs::Imu& msg){
     yaw_ = msg.orientation.z+2.181;
     //yaw_ *= -1;
 }
-void LLH2UTM(double Lat, double Long, double H){
+
+void LLH2UTM(double Lat, double Long, double H, geometry_msgs::PoseStamped& pose){
     double a = WGS84_A;
     double eccSquared = UTM_E2;
     double k0 = UTM_K0;
@@ -37,7 +39,7 @@ void LLH2UTM(double Lat, double Long, double H){
     double LatRad = angles::from_degrees(Lat);
     double LongRad = angles::from_degrees(LongTemp);
     double LongOriginRad;
-    cur_pose_.pose.position.z = H;
+    pose.pose.position.z = H;
     // Fix Zone number with Korea
     int zone = 52;
     char band = 'S';
@@ -56,11 +58,11 @@ void LLH2UTM(double Lat, double Long, double H){
         + (15*eccSquared*eccSquared/256
         + 45*eccSquared*eccSquared*eccSquared/1024)*sin(4*LatRad)
         - (35*eccSquared*eccSquared*eccSquared/3072)*sin(6*LatRad));
-    cur_pose_.pose.position.y = (double)
+    pose.pose.position.y = (double)
     (k0*N*(A+(1-T+C)*A*A*A/6
         + (5-18*T+T*T+72*C-58*eccPrimeSquared)*A*A*A*A*A/120)
     + 500000.0);
-    cur_pose_.pose.position.x = (double)
+    pose.pose.position.x = (double)
     (k0*(M+N*tan(LatRad)
         *(A*A/2+(5-T+9*C+4*C*C)*A*A*A*A/24
         + (61-58*T+T*T+600*C-330*eccPrimeSquared)*A*A*A*A*A*A/720)));
@@ -71,12 +73,171 @@ void LLH2UTM(double Lat, double Long, double H){
     {0.049875, 0.018177, -0.047063, -213252.081285},
     {0.000000, 0.000000, 0.000000, 1.000000}};
 
-    double input[4] = {cur_pose_.pose.position.x, cur_pose_.pose.position.y, cur_pose_.pose.position.z, 1};
-    cur_pose_.pose.position.x = TM[0][0]*input[0] + TM[0][1]*input[1] + TM[0][2]*input[2] + TM[0][3]*input[3];
-    cur_pose_.pose.position.y = TM[1][0]*input[0] + TM[1][1]*input[1] + TM[1][2]*input[2] + TM[1][3]*input[3];
-    cur_pose_.pose.position.z = TM[2][0]*input[0] + TM[2][1]*input[1] + TM[2][2]*input[2] + TM[2][3]*input[3];
+    double input[4] = {pose.pose.position.x, pose.pose.position.y, pose.pose.position.z, 1};
+    pose.pose.position.x = TM[0][0]*input[0] + TM[0][1]*input[1] + TM[0][2]*input[2] + TM[0][3]*input[3];
+    pose.pose.position.y = TM[1][0]*input[0] + TM[1][1]*input[1] + TM[1][2]*input[2] + TM[1][3]*input[3];
+    pose.pose.position.z = TM[2][0]*input[0] + TM[2][1]*input[1] + TM[2][2]*input[2] + TM[2][3]*input[3];    
+
+    // Add offset
+    Point p;
+    p.x = pose.pose.position.x;
+    p.y = pose.pose.position.y;
+    p.z = pose.pose.position.z;
+    add_offset(p);
+    pose.pose.position.x = p.x;
+    pose.pose.position.y = p.y;
+    pose.pose.position.z = p.z;
+}
+
+void init_zone_points(std::string filename){
+    ifstream inputFile(filename);
+    int l=0;
+
+    vector< vector<std::string> > data;
+
+    while(inputFile){
+        
+        string s;
+        if (!getline(inputFile, s)) break;
+        l++;
+        if (s[0] != '#') {
+            
+            istringstream ss(s);
+            vector<string> record;
+
+            while (ss) {
+                string line;
+                if (!getline(ss, line, ','))
+                    break;
+                try {
+                    record.push_back(line);
+                }
+                catch (const std::invalid_argument e) {
+                    cout << "NaN found in file " << filename << " line " << l
+                         << endl;
+                    e.what();
+                }
+            }
+            data.push_back(record);
+        }
+        else{
+            l--;
+        }
+    }
+    max_zone = l;
+
+    for(auto it = data.begin(); it != data.end(); ++it){
+        std::vector<string> data_line = *it;
+        std::vector<double> zone_line;
+        zone_line.push_back(stof(data_line[1])); // lt x
+        zone_line.push_back(stof(data_line[2])); // lt y
+        zone_line.push_back(stof(data_line[3])); // rt x
+        zone_line.push_back(stof(data_line[4])); // rt y
+        zone_line.push_back(stof(data_line[5])); // rb x
+        zone_line.push_back(stof(data_line[6])); // rb y
+        zone_line.push_back(stof(data_line[7])); // lb x
+        zone_line.push_back(stof(data_line[8])); // lb y
+        zone_points.push_back(zone_line);
+    }
 
 }
+
+void print_zone_points(std::vector< std::vector<double> >& zone_points){
+    std::cout<<"[Print Zone Points!]"<<std::endl;
+    for(auto it = zone_points.begin(); it != zone_points.end(); ++it){
+        for(int i = 0; i <8; ++i){
+            std::cout<<(*it)[i]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
+
+void init_zone_offset(std::string filename){
+    ifstream inputFile(filename);
+    int l=0;
+
+    vector< vector<std::string> > data;
+
+    while(inputFile){
+        l++;
+        string s;
+        if (!getline(inputFile, s)) break;
+        if (s[0] != '#') {
+            istringstream ss(s);
+            vector<string> record;
+
+            while (ss) {
+                string line;
+                if (!getline(ss, line, ','))
+                    break;
+                try {
+                    record.push_back(line);
+                }
+                catch (const std::invalid_argument e) {
+                    cout << "NaN found in file " << filename << " line " << l
+                         << endl;
+                    e.what();
+                }
+            }
+            data.push_back(record);
+        }
+    }
+
+    for(auto it = data.begin(); it != data.end(); ++it){
+        std::vector<string> data_line = *it;
+        std::vector<double> offset_line;
+        offset_line.push_back(stof(data_line[1])); // offset x
+        offset_line.push_back(stof(data_line[2])); // offset y
+        offset_line.push_back(stof(data_line[3])); // offset z
+        zone_offset.push_back(offset_line);
+    }
+
+}
+
+void print_zone_offset(std::vector< std::vector<double> >& zone_offset){
+    std::cout<<"[Print Zone Offset!]"<<std::endl;
+    for(auto it = zone_offset.begin(); it != zone_offset.end(); ++it){
+        for(int i = 0; i <3; ++i){
+            std::cout<<(*it)[i]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
+
+void add_offset(Point& p){
+    int isZoneFound = 0;
+    int target_zone = -1;
+    for(int i = 0; i < max_zone; i++){
+        
+        std::vector<double> line = zone_points[i];
+        std::vector<Point> polygon;
+        Point pol_point1 = {line[0], line[1], 0.0};
+        Point pol_point2 = {line[2], line[3], 0.0};
+        Point pol_point3 = {line[4], line[5], 0.0};
+        Point pol_point4 = {line[6], line[7], 0.0};
+        polygon.push_back(pol_point1);
+        polygon.push_back(pol_point2);
+        polygon.push_back(pol_point3);
+        polygon.push_back(pol_point4);
+
+        isZoneFound = PointInsidePolygon(polygon, p);
+
+        if(isZoneFound){
+            target_zone = i;
+            break;
+        }
+    }
+
+    if(DEBUG_FLAG) std::cout<<"Target Zone: "<<target_zone<<std::endl;
+    
+    if(isZoneFound){
+        p.x += zone_offset[target_zone][0];
+        p.y += zone_offset[target_zone][1];
+        p.z += zone_offset[target_zone][2];
+    }
+
+}
+
 void publishTF()
 {
   static tf::TransformBroadcaster br;
@@ -120,14 +281,40 @@ void publishVelocity(){
     prev_time_ = current_time_;
 }
 int main(int argc, char** argv){
+    std::string zone_points_filename;
+    std::string zone_offset_filename;
+
     ros::init(argc, argv, "gnss_cm_localizer");
     ros::NodeHandle nh;
     gps_sub = nh.subscribe("/gps_out", 100, GPSCallback);
     imu_sub = nh.subscribe("/imu_out", 100, IMUCallback);
     pose_pub = nh.advertise<geometry_msgs::PoseStamped>("/gnss_pose", 10);
     vel_pub = nh.advertise<geometry_msgs::TwistStamped>("gnss_vel", 10);    
+
     nh.param<bool>("/gnss_localizer_cm/publish_tf", publish_tf_, true);
+  
+    nh.param<std::string>("/gnss_localizer_cm/zone_points_filename", zone_points_filename, "none");
+    if(zone_points_filename == "none"){
+        ROS_ERROR("Can't find zone points file %s!", zone_points_filename.c_str());
+    }
+
+    nh.param<std::string>("/gnss_localizer_cm/zone_offset_filename", zone_offset_filename, "none");
+    if(zone_offset_filename == "none"){
+        ROS_ERROR("Can't find zone points file %s!", zone_offset_filename.c_str());
+    }
+
+    std::cout<<"================================"<<std::endl;
+    init_zone_points(zone_points_filename);
+    print_zone_points(zone_points);
+
+    std::cout<<endl;
+
+    init_zone_offset(zone_offset_filename);
+    print_zone_offset(zone_offset);
+    std::cout<<"================================"<<std::endl;
+
     while(ros::ok())
         ros::spin();
+
     return 0;
 }
--- rubis_ws/src/rubis_cm_pkg/src/waypoint_generator.cpp
+++ rubis_ws/src/rubis_cm_pkg/src/waypoint_generator.cpp
@@ -23,18 +23,74 @@
 #define UTM_E6		(UTM_E4*UTM_E2)		// e^6
 #define UTM_EP2		(UTM_E2/(1-UTM_E2))	// e'^2
 
+#define MIN(x,y) (x <= y ? x : y)
+#define MAX(x,y) (x >= y ? x : y)
+
 #define MAP_FRAME_ "map"
 #define GPS_FRAME_ "gps"
 
+#define DEBUG_FLAG true
 
 using namespace std;
 
+typedef struct _Point{
+    double x;
+    double y;
+    double z;
+}Point;
+
+static int max_zone = -1;
 ros::Publisher pub;
+static std::vector< std::vector<double> > zone_points;
+static std::vector< std::vector<double> > zone_offset;
+
 
 void LLH2UTM(double Lat, double Long, double H, geometry_msgs::Pose& pose);
-void create_waypoints(std::string filename, geometry_msgs::PoseArray& out)
+void create_waypoints(std::string waypoint_filename, geometry_msgs::PoseArray& out);
+void init_zone_points(std::string filename);
+void print_zone_points(std::vector< std::vector<double> >& zone_points);
+void init_zone_offset(std::string filename);
+void print_zone_offset(std::vector< std::vector<double> >& zone_points);
+void add_offset(Point& p);
+void test();
 
-;
+inline int PointInsidePolygon(const std::vector<Point>& polygon,const Point& p)
+{
+    int counter = 0;
+      int i;
+      double xinters;
+      Point p1,p2;
+      int N = polygon.size();
+      if(N <=0 ) return -1;
+
+      p1 = polygon.at(0);
+      for (i=1;i<=N;i++)
+      {
+        p2 = polygon.at(i % N);
+
+        if (p.y > MIN(p1.y,p2.y))
+        {
+          if (p.y <= MAX(p1.y,p2.y))
+          {
+            if (p.x <= MAX(p1.x,p2.x))
+            {
+              if (p1.y != p2.y)
+              {
+                xinters = (p.y-p1.y)*(p2.x-p1.x)/(p2.y-p1.y)+p1.x;
+                if (p1.x == p2.x || p.x <= xinters)
+                  counter++;
+              }
+            }
+          }
+        }
+        p1 = p2;
+      }
+
+      if (counter % 2 == 0)
+        return 0;
+      else
+        return 1;
+}
 
 void LLH2UTM(double Lat, double Long, double H, geometry_msgs::Pose& pose){
     double a = WGS84_A;
@@ -88,14 +144,85 @@ void LLH2UTM(double Lat, double Long, double H, geometry_msgs::Pose& pose){
     pose.position.y = TM[1][0]*input[0] + TM[1][1]*input[1] + TM[1][2]*input[2] + TM[1][3]*input[3];
     pose.position.z = TM[2][0]*input[0] + TM[2][1]*input[1] + TM[2][2]*input[2] + TM[2][3]*input[3];    
 
+    // Add offset
+    Point p;
+    p.x = pose.position.x;
+    p.y = pose.position.y;
+    p.z = pose.position.z;
+    add_offset(p);
+    pose.position.x = p.x;
+    pose.position.y = p.y;
+    pose.position.z = p.z;
 }
 
-void create_waypoints(std::string filename, geometry_msgs::PoseArray& out){
-    static int seq;
+void init_zone_points(std::string filename){
     ifstream inputFile(filename);
     int l=0;
 
-    vector< vector<string> > data;
+    vector< vector<std::string> > data;
+
+    while(inputFile){
+        
+        string s;
+        if (!getline(inputFile, s)) break;
+        l++;
+        if (s[0] != '#') {
+            
+            istringstream ss(s);
+            vector<string> record;
+
+            while (ss) {
+                string line;
+                if (!getline(ss, line, ','))
+                    break;
+                try {
+                    record.push_back(line);
+                }
+                catch (const std::invalid_argument e) {
+                    cout << "NaN found in file " << filename << " line " << l
+                         << endl;
+                    e.what();
+                }
+            }
+            data.push_back(record);
+        }
+        else{
+            l--;
+        }
+    }
+    max_zone = l;
+
+    for(auto it = data.begin(); it != data.end(); ++it){
+        std::vector<string> data_line = *it;
+        std::vector<double> zone_line;
+        zone_line.push_back(stof(data_line[1])); // lt x
+        zone_line.push_back(stof(data_line[2])); // lt y
+        zone_line.push_back(stof(data_line[3])); // rt x
+        zone_line.push_back(stof(data_line[4])); // rt y
+        zone_line.push_back(stof(data_line[5])); // rb x
+        zone_line.push_back(stof(data_line[6])); // rb y
+        zone_line.push_back(stof(data_line[7])); // lb x
+        zone_line.push_back(stof(data_line[8])); // lb y
+        zone_points.push_back(zone_line);
+    }
+
+}
+
+void print_zone_points(std::vector< std::vector<double> >& zone_points){
+    std::cout<<"[Print Zone Points!]"<<std::endl;
+    for(auto it = zone_points.begin(); it != zone_points.end(); ++it){
+        for(int i = 0; i <8; ++i){
+            std::cout<<(*it)[i]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
+
+void init_zone_offset(std::string filename){
+    ifstream inputFile(filename);
+    int l=0;
+
+    vector< vector<std::string> > data;
 
     while(inputFile){
         l++;
@@ -122,11 +249,97 @@ void create_waypoints(std::string filename, geometry_msgs::PoseArray& out){
         }
     }
 
+    for(auto it = data.begin(); it != data.end(); ++it){
+        std::vector<string> data_line = *it;
+        std::vector<double> offset_line;
+        offset_line.push_back(stof(data_line[1])); // offset x
+        offset_line.push_back(stof(data_line[2])); // offset y
+        offset_line.push_back(stof(data_line[3])); // offset z
+        zone_offset.push_back(offset_line);
+    }
+
+}
+
+void print_zone_offset(std::vector< std::vector<double> >& zone_offset){
+    std::cout<<"[Print Zone Offset!]"<<std::endl;
+    for(auto it = zone_offset.begin(); it != zone_offset.end(); ++it){
+        for(int i = 0; i <3; ++i){
+            std::cout<<(*it)[i]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
+
+void add_offset(Point& p){
+    int isZoneFound = 0;
+    int target_zone = -1;
+    for(int i = 0; i < max_zone; i++){
+        
+        std::vector<double> line = zone_points[i];
+        std::vector<Point> polygon;
+        Point pol_point1 = {line[0], line[1], 0.0};
+        Point pol_point2 = {line[2], line[3], 0.0};
+        Point pol_point3 = {line[4], line[5], 0.0};
+        Point pol_point4 = {line[6], line[7], 0.0};
+        polygon.push_back(pol_point1);
+        polygon.push_back(pol_point2);
+        polygon.push_back(pol_point3);
+        polygon.push_back(pol_point4);
+
+        isZoneFound = PointInsidePolygon(polygon, p);
+
+        if(isZoneFound){
+            target_zone = i;
+            break;
+        }
+    }
+
+    if(DEBUG_FLAG) std::cout<<"Target Zone: "<<target_zone<<std::endl;
+    if(isZoneFound){
+        p.x += zone_offset[target_zone][0];
+        p.y += zone_offset[target_zone][1];
+        p.z += zone_offset[target_zone][2];
+    }
+
+}
+
+void create_waypoints(std::string waypoint_filename, geometry_msgs::PoseArray& out){
+    static int seq;
+    ifstream inputFile(waypoint_filename);
+    int l=0;
+
+    vector< vector<string> > data;
+
+    while(inputFile){
+        l++;
+        string s;
+        if (!getline(inputFile, s))
+            break;
+        if (s[0] != '#') {
+            istringstream ss(s);
+            vector<string> record;
+
+            while (ss) {
+                string line;
+                if (!getline(ss, line, ','))
+                    break;
+                try {
+                    record.push_back(line);
+                }
+                catch (const std::invalid_argument e) {
+                    cout << "NaN found in file " << waypoint_filename << " line " << l
+                         << endl;
+                    e.what();
+                }
+            }
+            data.push_back(record);
+        }
+    }
+
     out.header.stamp = ros::Time::now();
     out.header.seq = seq;
     out.header.frame_id = "map";
 
-    std::cout<<"========================="<<std::endl;
     for(auto it = data.begin(); it != data.end(); ++it){
         vector<string> line = *it;
         geometry_msgs::Pose pose;
@@ -137,26 +350,97 @@ void create_waypoints(std::string filename, geometry_msgs::PoseArray& out){
         LLH2UTM(lat, lon, h, pose);
         out.poses.push_back(pose);
     }
-    std::cout<<out.poses.size()<<std::endl;
     pub.publish(out);
+}
+
+void test(){
+    Point z1 = {1920.635,   -990.108,   -0.276};
+    Point z2 = {1934.372,	-670.724,	-5.613};
+    Point z3 = {1944.662,	-350.788,	-10.75};
+    Point z4 = {1955.783,	-54.138,	-15.609};
+    Point z5 = {1963.3,	    146.549,	-18.949};
+    Point z6 = {1973.047,	405.937,	-23.696};
+    Point z7 = {1982.455,	700.686,	-27.982};
+    Point z8 = {1694.887,	725.675,	-13.846};
+    Point z9 = {1660.789,	456.817,	-8.263};
+    Point z10 = {1632.052,	177.283,	-2.567};
+    Point z11 = {1612.564,	-20.432,	1.128};
+    Point z12 = {1605.283,	-320.892,	5.86};
+    Point z13 = {1631.438,	-644.327,	9.209};
+    Point z14 = {1652.356,	-920.857,	11.93};
+    Point z15 = {2272.361,	-55.788,	-31.722};
+    Point z16 = {2288.847,	137.241,	-35.229};
+    Point z17 = {2252.821,	351.01,	    -37.476};
+    
+    std::vector<Point> test_data;
+    test_data.push_back(z1);
+    test_data.push_back(z2);
+    test_data.push_back(z3);
+    test_data.push_back(z4);
+    test_data.push_back(z5);
+    test_data.push_back(z6);
+    test_data.push_back(z7);
+    test_data.push_back(z8);
+    test_data.push_back(z9);
+    test_data.push_back(z10);
+    test_data.push_back(z11);
+    test_data.push_back(z12);
+    test_data.push_back(z13);
+    test_data.push_back(z14);
+    test_data.push_back(z15);
+    test_data.push_back(z16);
+    test_data.push_back(z17);
+    
+    for(int i = 0; i < 17; i++){
+        std::cout<<"[Zone "<<i<<"]"<<std::endl;
+        std::cout<<"Before - "<<test_data[i].x<<" "<<test_data[i].y<<" "<<test_data[i].z<<std::endl;
+        add_offset(test_data[i]);
+        std::cout<<"After - "<<test_data[i].x<<" "<<test_data[i].y<<" "<<test_data[i].z<<std::endl;
+    }
+    std::cout<<"================================"<<std::endl;
+
 }
 
 int main(int argc, char* argv[]){
     ros::init(argc, argv, "waypoint_generator");
     ros::NodeHandle nh;
-    std::string filename;
+    std::string waypoint_filename;
+    std::string zone_points_filename;
+    std::string zone_offset_filename;
     ros::Rate r(10);
 
     pub = nh.advertise<geometry_msgs::PoseArray>("/global_waypoints", 1);
 
-    nh.param<std::string>("/waypoint_generator/filename", filename, "none");
-    if(filename == "none"){
-        ROS_ERROR("Can't find open file %s!", filename.c_str());
+    nh.param<std::string>("/waypoint_generator/waypoint_filename", waypoint_filename, "none");
+    if(waypoint_filename == "none"){
+        ROS_ERROR("Can't find waypoint file %s!", waypoint_filename.c_str());
     }
+
+    nh.param<std::string>("/waypoint_generator/zone_points_filename", zone_points_filename, "none");
+    if(zone_points_filename == "none"){
+        ROS_ERROR("Can't find zone points file %s!", zone_points_filename.c_str());
+    }
+
+    nh.param<std::string>("/waypoint_generator/zone_offset_filename", zone_offset_filename, "none");
+    if(zone_offset_filename == "none"){
+        ROS_ERROR("Can't find zone points file %s!", zone_offset_filename.c_str());
+    }
+
+    std::cout<<"================================"<<std::endl;
+    init_zone_points(zone_points_filename);
+    print_zone_points(zone_points);
+
+    std::cout<<endl;
+
+    init_zone_offset(zone_offset_filename);
+    print_zone_offset(zone_offset);
+    std::cout<<"================================"<<std::endl;
+
+    if(DEBUG_FLAG) test();
 
     while(ros::ok()){
         geometry_msgs::PoseArray out;
-        create_waypoints(filename, out);
+        create_waypoints(waypoint_filename, out);
         r.sleep();
     }
 
